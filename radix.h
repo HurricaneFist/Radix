3414fdf632338080daac2e5049bf6dbc44f606db
Ian <pyropter28@gmail.com>
2014-05-28 10:57:12 -0500

    made DIGITS const

--- radix.h
+++ radix.h
@@ -11,18 +11,20 @@
  * 
  * https://github.com/LastWhisper
  */
+ 
+const std::string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 std::string convert(unsigned long long n, int radix) {
 	if (n == 0)
 		return "0"; // This is necessary since logf(0) creates a wormhole
 
-	std::string number, digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	std::string number;
 	
 	unsigned long long j = n, k = n;
 	for (int i = (int)(logf(n)/logf(radix)); i >= 0; i--) {
 		j = k;
 		k %= (unsigned long long) pow(radix, i);
-		number += digits[(j - k) / (unsigned long long) pow(radix, i)];
+		number += DIGITS[(j - k) / (unsigned long long) pow(radix, i)];
 	}
 	
 	return number;
@@ -30,10 +32,9 @@ std::string convert(unsigned long long n, int radix) {
 
 unsigned long long revert(std::string number, int radix) {
 	unsigned long long n = 0;
-	std::string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
 	for (int i = 0; i < number.size(); i++)
-		n += digits.find(number[i]) * (unsigned long long) pow(radix, number.size()-1-i);
+		n += DIGITS.find(number[i]) * (unsigned long long) pow(radix, number.size()-1-i);
 		
 	return n;
 }
