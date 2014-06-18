04f9cc3b47cb872579ccccc61d9688fe0f765443
Ian <pyropter28@gmail.com>
2014-06-18 09:55:40 -0500

    Created a namespace

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << convert(1000, 16)        << std::endl  // prints "3E8"
-	          << revert("3E8", 16)        << std::endl  // prints "1000"
-	          << transvert("3E8", 16, 18) << std::endl; // prints "31A"
+	std::cout << Radix::convert(1000, 16)        << std::endl  // prints "3E8"
+	          << Radix::revert("3E8", 16)        << std::endl  // prints "1000"
+	          << Radix::transvert("3E8", 16, 18) << std::endl; // prints "31A"
 }
--- radix.h
+++ radix.h
@@ -10,42 +10,45 @@
  * 
  * https://github.com/HurricaneFist
  */
- 
-const std::string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-std::string convert(unsigned long long n, int radix) {
-	if (n == 0)
-		return "0"; // This is necessary since logf(0) creates a wormhole
+namespace Radix {
+
+	const std::string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	std::string convert(unsigned long long n, int radix) {
+		if (n == 0)
+			return "0"; // This is necessary since logf(0) creates a wormhole
+			
+		std::string number;
 		
-	std::string number;
-	
-	if (radix == 1) {
-		for (int i = 0; i < n; i++)
-			number += "1";
+		if (radix == 1) {
+			for (int i = 0; i < n; i++)
+				number += "1";
+			return number;
+		}
+		
+		unsigned long long j = n, k = n;
+		for (int i = (int)(logf(n)/logf(radix)); i >= 0; i--) {
+			j = k;
+			k %= (unsigned long long) pow(radix, i);
+			number += DIGITS[(j - k) / (unsigned long long) pow(radix, i)];
+		}
+		
 		return number;
 	}
-	
-	unsigned long long j = n, k = n;
-	for (int i = (int)(logf(n)/logf(radix)); i >= 0; i--) {
-		j = k;
-		k %= (unsigned long long) pow(radix, i);
-		number += DIGITS[(j - k) / (unsigned long long) pow(radix, i)];
-	}
-	
-	return number;
-}
 
-unsigned long long revert(std::string number, int radix) {
-	unsigned long long n = 0;
-	
-	for (int i = 0; i < number.size(); i++)
-		n += DIGITS.find(number[i]) * (unsigned long long) pow(radix, number.size()-1-i);
+	unsigned long long revert(std::string number, int radix) {
+		unsigned long long n = 0;
 		
-	return n;
-}
-        
-std::string transvert(std::string number, int original_radix, int new_radix) {
-	return convert(revert(number, original_radix), new_radix);
+		for (int i = 0; i < number.size(); i++)
+			n += DIGITS.find(number[i]) * (unsigned long long) pow(radix, number.size()-1-i);
+			
+		return n;
+	}
+			
+	std::string transvert(std::string number, int original_radix, int new_radix) {
+		return convert(revert(number, original_radix), new_radix);
+	}
 }
 
 #endif
